11e10ba5dba7fc31d5fcfbfc209be2a4d59b207a
rhodinemma <rhodinemma10@gmail.com>
2023-09-28 19:00:15 +0300

    chore: refactor by extracting sub components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css';
+import { Todo } from './Todo';
 
 function App() {
   return (
     <div className="App">
       Learn React in 5 minutes
+
+      <Todo />
     </div>
   );
 }
--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,33 +1,59 @@
-import { useState } from "react"
-import { v4 as uuid } from "uuid"
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
 
 type TodoType = {
     id: string;
     content: string;
+};
+
+const TodoList = ({ todos }: { todos: TodoType[] }) => {
+    return (
+        <>
+            {todos.map((todo) => (
+                <div key={todo.id}>{todo.content}</div>
+            ))}
+        </>
+    )
+}
+
+const TodoInput = ({ onItemAdded }: { onItemAdded: (todo: TodoType) => void }) => {
+    const [content, setContent] = useState<string>("");
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setContent(e.target.value);
+    };
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter") {
+            const id = uuid();
+            onItemAdded({ id, content });
+        }
+    };
+
+    return (
+        <input
+            type="text"
+            data-testid="todo-input"
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+        />
+    )
 }
 
 const Todo = () => {
     const [todos, setTodos] = useState<TodoType[]>([]);
-    const [content, setContent] = useState<string>('');
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setContent(e.target.value);
+    const onItemAdded = (todo: TodoType) => {
+        setTodos([todo, ...todos]);
     }
 
-    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === 'Enter') {
-            const id = uuid()
-            setTodos([{ id, content }, ...todos]);
-        }
-    }
+    return (
+        <div>
+            <h2>todos</h2>
+            <TodoInput onItemAdded={onItemAdded} />
+            <TodoList todos={todos} />
+        </div>
+    );
+};
 
-    return <div>
-        <h2>todos</h2>
-        <input type="text" data-testid="todo-input" onChange={handleChange} onKeyDown={handleKeyDown} />
-
-        {todos.map(todo => <div key={todo.id}>{todo.content}</div>)}
-    </div>
-
-}
-
-export { Todo }
+export { Todo };
